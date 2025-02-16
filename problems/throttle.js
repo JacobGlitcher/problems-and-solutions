cb588953df51232a5e153da7c03a9daf6b3c36b0
JacobGlitcher <hakob.yer@gmail.com>
2025-02-16 16:41:43 +0400

    throttle problem and showcase

--- problems/throttle.js
+++ problems/throttle.js
@@ -1,1 +1,20 @@
+function throttle(func, limit) {
+  let inThrottle = false;
 
+  return function (...args) {
+    if (!inThrottle) {
+      inThrottle = true;
+
+      setTimeout(() => {
+        func.apply(this, args)
+
+        inThrottle = false;
+      }, limit)
+    }
+  };
+}
+
+const logOnMouseEvent = () => console.log('Mouse Moved');
+const throttleOnMouseMove = throttle(logOnMouseEvent, 1000)
+
+window.addEventListener('mousemove', throttleOnMouseMove);
