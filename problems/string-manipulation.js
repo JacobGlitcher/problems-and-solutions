7bb8581b4e66325f70bb77136ae79f29028b8b38
JacobGlitcher <hakob.yer@gmail.com>
2025-02-17 23:05:19 +0400

    added check for palindrome functions

---
+++ problems/string-manipulation.js
@@ -0,0 +1,76 @@
+// reverse string
+function reverseString(str) {
+  return str.split('').reverse().join('')
+}
+
+console.log(reverseString('Hello World!'))
+
+function reverseStringManual(str) {
+  let result = '';
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+
+  return result;
+}
+
+console.log(reverseStringManual('Hello World!'))
+
+function reverseStringRecursive(str) {
+  if (str.length < 1) return str
+
+  return str.slice(-1) + reverseStringRecursive(str.slice(0, -1))
+}
+
+console.log(reverseStringRecursive('Hello World!'))
+
+function reverseUnicodeString(str) {
+  return [...str].reverse().join('')
+}
+
+console.log(reverseUnicodeString('👋🌍'))
+
+// check palindrome
+function isPalindromeBasic(str) {
+  return str.split('').reverse().join('') === str
+}
+
+console.log(isPalindromeBasic('ata'))
+
+function isPalindromeManual(str) {
+  let reversedString = ''
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedString += str[i]
+  }
+
+  return reversedString === str
+}
+
+console.log(isPalindromeManual('ata'))
+
+function isPalindromeAdvanced(str) {
+  str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+  if (str.length <= 1) return true;
+
+  let left = 0
+  let right = str.length - 1
+
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false
+    }
+
+    left++
+    right--
+  }
+
+  return true;
+}
+
+console.log(isPalindromeAdvanced('ata'))
+
+
+
+
